f80055f1023f18055f09875266f820440901a0eb
Muhammad Ubaid Raza <mubaidr@gmail.com>
2016-12-11 22:26:10 +0500

    add ad-blocker warn script

--- functions.php
+++ functions.php
@@ -1,12 +1,25 @@
 <?php
-function my_theme_enqueue_styles() {
-    $parent_style = 'parent-style';
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
-}
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+    function my_theme_enqueue_styles() {
+        $parent_style = 'parent-style';
+        wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+        
+        wp_enqueue_style( 'child-style',
+            get_stylesheet_directory_uri() . '/style.css',
+            array( $parent_style ),
+            wp_get_theme()->get('Version')
+        );
+
+        wp_enqueue_script(
+            'block-adblock-script',
+            get_stylesheet_directory_uri() . '/js/blockadblock.js',
+            array( 'blockadblock' )
+        );
+
+        wp_enqueue_script(
+            'pakgamers_script',
+            get_stylesheet_directory_uri() . '/js/pakgamers_script.js',
+            array( 'pakgamers_script' )
+        );
+    }
+    add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 ?>
